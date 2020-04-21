4ac44c102f3a8a0428dc72da34ba3433345b2bfe
liwang <ethan@egenie.cn>
2020-04-21 09:58:55 +0800

    update

---
+++ common.js
@@ -0,0 +1,28 @@
+/**
+ * 获取指定格式的日期和时间
+ * @param dt
+ * @returns {string}
+ */
+function getDate(dt){
+    var year = dt.getFullYear();
+    var month = dt.getMonth()+1;
+    var day = dt.getDate();
+    var hour = dt.getHours();
+    var minute = dt.getMinutes();
+    var second = dt.getSeconds();
+    month = zifull(month);
+    day = zifull(day);
+    hour = zifull(hour);
+    minute = zifull(minute);
+    second = zifull(second);
+    return year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second;
+}
+
+/**
+ * 自动补0
+ * @param s
+ * @returns {string}
+ */
+function zifull(s){
+    return s<10?"0"+s:s;
+}
